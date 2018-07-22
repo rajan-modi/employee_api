c0627754a5b0bd611c1e562ee81bd0ee1b740223
Rajan <rajan.modi@zerebral.co.in>
2018-07-22 22:33:38 +0530

    total_employees in response added

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@
 
     var pagination = require('pagination');
 
+    var total_employees;
+
     router.get('/', function (req, res) {
         res.send('{"200" : "OK"}');
         res.end();
@@ -78,12 +80,39 @@
           var page_size = 10;
           offset = (page_no-1) * page_size;
           console.log(page_no);
+
+          db.query('SELECT count(*) AS total_employees FROM `employee`', function (error, result, fields) {
+                      if (error)
+                      {
+                          res.status(500).json(
+                              {
+                                  "status_code": 500,
+                                  "status_message": "Internal server error"
+                              });
+                      }
+                      console.log(result[0].total_employees);
+                      total_employees = result[0].total_employees;
+                      console.log(total_employees);
+                    });
+
           var find_employee = 'SELECT `emp_no`,`first_name`,`last_name` FROM `employee` ORDER BY `emp_no` limit ?,10';
 
           db.query(find_employee, [offset], function (error, rows, fields) {
-            if (error) { res.status(500).json({"status_code": 500, "status_message": "Internal server error"}); }
-
-            res.send(JSON.stringify(rows));
+            if (error)
+            {
+                res.status(500).json(
+                    {
+                        "status_code": 500,
+                        "status_message": "Internal server error"
+                    });
+            }
+            //res.send(JSON.stringify(rows));
+            res.send(
+                {
+                    "total_employees": total_employees,
+                    "employees_data": rows
+                }
+            );
             res.end();
           });
         })
